cc1f6a0d7bfdeedd4359e74d21f1801f2f49dc36
SaiShashankGoud <144211877+SaiShashankGoud@users.noreply.github.com>
2023-12-27 10:03:33 +0530

    Update App.java

--- src/main/java/SE/maven/App.java
+++ src/main/java/SE/maven/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        a=9,b=10;
+        c=a+b;
+        System.out.println( "a+b="+c);
         System.out.println( "Hello World!" );
         System.out.println( "Hello you!" );
         System.out.println( "Hello eueueu!" );
